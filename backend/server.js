a81adc9ad25e8e81b218c17c185cfa1b758ed673
Jaanvi Chouhan <jaanvichouhan18805@gmail.com>
2026-04-04 20:36:29 +0530

    Fix hardcoded localhost - connect to Render backend

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,14 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: '*',
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://cj-ftkytvay9-jaanvi-chouhans-projects.vercel.app'
+  ],
   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
 
 app.options('*', cors());
